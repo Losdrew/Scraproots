f17c2c6798c563e1fc59bb4cbe58573741de1fcd
Artem Volovyi <artem.volovyi@gmail.com>
2024-12-21 23:43:34 +0200

    Add inventory items change event

---
+++ Source/Scraproots/Private/Core/SRGameplayTags.cpp
@@ -0,0 +1,7 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Core/SRGameplayTags.h"
+
+const FGameplayTag FSRTag_Event::Inventory_ItemsChanged = FGameplayTag::RequestGameplayTag(FName("Event.Inventory.ItemsChanged"));
---
+++ Source/Scraproots/Public/Core/SRGameplayTags.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameplayTagContainer.h"
+#include "SRGameplayTags.generated.h"
+
+/**
+ *  Centralized location for all gameplay tags used in C++ code.
+ */
+USTRUCT(BlueprintType)
+struct SCRAPROOTS_API FSRTag_Event
+{
+	GENERATED_BODY()
+
+	static const FGameplayTag Inventory_ItemsChanged;
+};
