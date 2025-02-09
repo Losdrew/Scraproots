c491dc26875daf252147e2f73874ebd07889cd62
NurePershynaAnastasiia <anastasiia.pershyna@nure.ua>
2025-02-09 15:27:19 +0200

    Update Obstacle random generation

--- Source/Scraproots/Private/ObstacleCoord.cpp
+++ Source/Scraproots/Private/ObstacleCoord.cpp
@@ -5,18 +5,10 @@ USRObstacleCoord::USRObstacleCoord()
 {
 }
 
-TArray<TArray<bool>> USRObstacleCoord::CreateBoolGrid(int32 GridSizeX, int32 GridSizeY) const
+TArray<TArray<bool>> USRObstacleCoord::InitializeGrid(int32 GridSizeX, int32 GridSizeY) const
 {
 	TArray<TArray<bool>> Grid;
-
-	if (GridSizeX <= 0 || GridSizeY <= 0)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("Invalid grid size! GridSizeX: %d, GridSizeY: %d"), GridSizeX, GridSizeY);
-		return Grid;  // Повертаємо порожній масив
-	}
-
 	Grid.SetNum(GridSizeX);
-
 	for (int32 X = 0; X < GridSizeX; X++)
 	{
 		Grid[X].SetNum(GridSizeY);
@@ -25,47 +17,57 @@ TArray<TArray<bool>> USRObstacleCoord::CreateBoolGrid(int32 GridSizeX, int32 Gri
 			Grid[X][Y] = false;
 		}
 	}
-
 	return Grid;
 }
 
-
-TArray<int32> USRObstacleCoord::GetRandomObstacleCoord(int32 GridSizeX, int32 GridSizeY) const
+void USRObstacleCoord::GenerateObstacles(TArray<TArray<bool>>& Grid, int32 GridSizeX, int32 GridSizeY, int MinObstacles, int MaxObstacles)
 {
-	int cellsTotal = GridSizeX * GridSizeY;
-	int minObstacles = round((double)cellsTotal * 0.3);
-	int maxObstacles = round((double)cellsTotal * 0.5);
-	int obstaclesTotal = 0;
+	int ObstaclesTotal = 0;
 
-	TArray<TArray<bool>> Grid = CreateBoolGrid(GridSizeX, GridSizeY);
-
-	while (obstaclesTotal < minObstacles)
+	while (ObstaclesTotal < MinObstacles)
 	{
-		for (int X = 0; X < GridSizeX; X++)
+		for (int32 X = 0; X < GridSizeX; X++)
 		{
-			for (int Y = 1; Y < GridSizeY; Y++)
+			for (int32 Y = 1; Y < GridSizeY; Y++)  // Start from Y = 1
 			{
-				int rndNum = FMath::RandRange(1, 10);
-				if (rndNum == 1 && obstaclesTotal < maxObstacles && !Grid[X][Y])
+				int RndNum = FMath::RandRange(1, 10);
+				if (RndNum == 1 && ObstaclesTotal < MaxObstacles && !Grid[X][Y])
 				{
 					Grid[X][Y] = true;
-					obstaclesTotal++;
+					ObstaclesTotal++;
 				}
 			}
 		}
 	}
+}
 
-	TArray<int32> ResultArray;
-	for (int X = 0; X < GridSizeX; X++)
+TArray<FObstacleCoord> USRObstacleCoord::ConvertGridToArray(const TArray<TArray<bool>>& Grid, int32 GridSizeX, int32 GridSizeY)
+{
+	TArray<FObstacleCoord> ResultArray;
+
+	for (int32 X = 0; X < GridSizeX; X++)
 	{
-		for (int Y = 0; Y < GridSizeY; Y++)
+		for (int32 Y = 1; Y < GridSizeY; Y++)  // Start from Y = 1
 		{
 			if (Grid[X][Y])
 			{
-				ResultArray.Add(Y + X * 1000);
+				ResultArray.Add(FObstacleCoord(X * 200, Y * 200));	// Store as a struct
 			}
 		}
 	}
 
 	return ResultArray;
+}
+
+TArray<FObstacleCoord> USRObstacleCoord::GetRandomObstacleCoord(int32 GridSizeX, int32 GridSizeY)
+{
+	int CellsTotal = GridSizeX * GridSizeY;
+	int MinObstacles = round((double)CellsTotal * 0.25);
+	int MaxObstacles = round((double)CellsTotal * 0.35);
+
+	TArray<TArray<bool>> Grid = InitializeGrid(GridSizeX, GridSizeY);
+
+	GenerateObstacles(Grid, GridSizeX, GridSizeY, MinObstacles, MaxObstacles);
+
+	return ConvertGridToArray(Grid, GridSizeX, GridSizeY);
 }
--- Source/Scraproots/Public/ObstacleCoord.h
+++ Source/Scraproots/Public/ObstacleCoord.h
@@ -4,6 +4,26 @@
 #include "UObject/Object.h"
 #include "ObstacleCoord.generated.h"
 
+USTRUCT(BlueprintType)
+struct FObstacleCoord
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadWrite, Category = "Obstacle")
+	int32 X;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Obstacle")
+	int32 Y;
+
+	// Default Constructor
+	FObstacleCoord()
+		: X(0), Y(0) {}
+
+	// Parameterized Constructor
+	FObstacleCoord(int32 InX, int32 InY)
+		: X(InX), Y(InY) {}
+};
+
 UCLASS(Blueprintable, BlueprintType)
 class SCRAPROOTS_API USRObstacleCoord : public UObject
 {
@@ -13,8 +33,11 @@ public:
 	USRObstacleCoord();
 
 	UFUNCTION(BlueprintCallable, Category = "Obstacle")
-	TArray<int32> GetRandomObstacleCoord(int32 GridSizeX, int32 GridSizeY) const;
+	TArray<FObstacleCoord> GetRandomObstacleCoord(int32 GridSizeX, int32 GridSizeY);
 
 private:
 	TArray<TArray<bool>> CreateBoolGrid(int32 GridSizeX, int32 GridSizeY) const;
+	TArray<TArray<bool>> InitializeGrid(int32 GridSizeX, int32 GridSizeY) const;
+	void GenerateObstacles(TArray<TArray<bool>>& Grid, int32 GridSizeX, int32 GridSizeY, int MinObstacles, int MaxObstacles);
+	TArray<FObstacleCoord> ConvertGridToArray(const TArray<TArray<bool>>& Grid, int32 GridSizeX, int32 GridSizeY);
 };
