82c9e6d24bc6dc052c43f2359dbf967a49a5b284
NurePershynaAnastasiia <anastasiia.pershyna@nure.ua>
2025-02-19 15:10:07 +0200

    Add random spawn for ally units

---
+++ Source/Scraproots/Private/GenerateAllyTileIndices.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GenerateAllyTileIndices.h"
+#include "Math/UnrealMathUtility.h"
+
+USRGenerateAllyTileIndices::USRGenerateAllyTileIndices()
+{
+}
+
+TArray<int32> USRGenerateAllyTileIndices::GenerateUniqueRandomNumbers(int32 NumberOfUnits, int32 GridYSize)
+{
+	TArray<int32> PossibleIndices;
+	TArray<int32> RandomAllyIndices;
+
+	// Заповнюємо PossibleNumbers числами від 0 до M
+	for (int32 i = 0; i < GridYSize; i++)
+	{
+		PossibleIndices.Add(i);
+	}
+
+	// Перемішуємо PossibleNumbers (Fisher-Yates Shuffle)
+	for (int32 i = PossibleIndices.Num() - 1; i > 0; i--)
+	{
+		int32 j = FMath::RandRange(0, i);
+		PossibleIndices.Swap(i, j);
+	}
+
+	// Додаємо перші N унікальних елементів у RandomNumbers
+	for (int32 i = 0; i < NumberOfUnits && i < PossibleIndices.Num(); i++)
+	{
+		RandomAllyIndices.Add(PossibleIndices[i] * 1000);
+	}
+
+	return RandomAllyIndices;
+}
---
+++ Source/Scraproots/Public/GenerateAllyTileIndices.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "GenerateAllyTileIndices.generated.h"
+
+
+UCLASS(Blueprintable, BlueprintType)
+class SCRAPROOTS_API USRGenerateAllyTileIndices : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	USRGenerateAllyTileIndices();
+
+	UFUNCTION(BlueprintCallable, Category = "Random Generation")
+	static TArray<int32> GenerateUniqueRandomNumbers(int32 NumberOfUnits, int32 GridYSize);
+};
